7993ceb4d89bad087bc412391ac64bfef2402060
rafa <jerhemoso18@gmail.com>
2022-05-21 04:01:00 -0300

    aparentemente o algoritmo genetico puro esta funcionando. Tem que ser testado

--- gatsp/src/genetic.rs
+++ gatsp/src/genetic.rs
@@ -1,4 +1,5 @@
 use rand::seq::SliceRandom;
+use rand::Rng;
 
 pub fn distance(ax: f64, ay: f64, bx: f64, by: f64) -> f64 {
     let x = bx - ax;
@@ -56,4 +57,100 @@ pub fn calcular_fitness(
         fitness.push(1.0 / value_of_cromossome(i, tam, cities));
     }
     return fitness;
+}
+
+pub fn selecao_por_roleta(fitness: Vec<f64>) -> i32 {
+    let sum_fitness: f64 = fitness.iter().sum();
+    let mut prob: Vec<f64> = Vec::new();
+    for i in fitness {
+        prob.push(i / sum_fitness);
+    }
+
+    let mut offset: f64 = 0.0;
+    let mut pick: i32 = 0;
+
+    let mut c: i32 = 0;
+
+    let mut rng = rand::thread_rng();
+    let rand_choice = rng.gen_range(0.0..1.0);
+    while (c as usize) < prob.len() {
+        offset += prob[c as usize];
+        if rand_choice < offset {
+            pick = c;
+            break;
+        }
+        c += 1;
+    }
+    return pick;
+}
+
+pub fn cruzamento_cx(pai1: Vec<i32>, pai2: Vec<i32>, cities: &mut Vec<[f64; 2]>) -> Vec<i32> {
+    let mut filho1: Vec<i32>;
+    let mut filho2: Vec<i32>;
+
+    filho1 = pai2.clone();
+    let mut c: usize = 0;
+    filho1[c as usize] = pai1[c as usize];
+    c = pai2.iter().position(|&r| r == filho1[c]).unwrap();
+    while (c as usize) != 0 {
+        filho1[c as usize] = pai1[c as usize];
+        c = pai2.iter().position(|&r| r == filho1[c]).unwrap();
+    }
+
+    filho2 = pai1.clone();
+    c = 0;
+    filho2[c as usize] = pai2[c as usize];
+    c = pai1.iter().position(|&r| r == filho2[c]).unwrap();
+    while (c as usize) != 0 {
+        filho2[c as usize] = pai2[c as usize];
+        c = pai1.iter().position(|&r| r == filho2[c]).unwrap();
+    }
+
+    if value_of_cromossome(filho1.clone(), filho1.len(), cities)
+        < value_of_cromossome(filho2.clone(), filho2.len(), cities)
+    {
+        return filho2;
+    } else {
+        return filho1;
+    }
+}
+
+pub fn pior_cromossomo(fitness: Vec<f64>) -> usize {
+    let mut minvalues: f64 = fitness[0];
+    let mut minidx: usize = 0;
+    while minidx < fitness.len() {
+        if minvalues > fitness[minidx] {
+            minvalues = fitness[minidx];
+            return minidx;
+        }
+        minidx += 1;
+    }
+
+    return 0;
+}
+
+pub fn melhor_cromossomo(fitness: Vec<f64>) -> usize {
+    let mut maxvalues: f64 = fitness[0];
+    let mut maxidx: usize = 0;
+    while maxidx < fitness.len() {
+        if maxvalues < fitness[maxidx] {
+            maxvalues = fitness[maxidx];
+            return maxidx;
+        }
+        maxidx += 1;
+    }
+
+    return 0;
+}
+
+pub fn mutacao(alvo: &mut Vec<i32>) {
+    let mut rng = rand::thread_rng();
+    let rand_choice1 = rng.gen_range(0..alvo.len());
+    let mut rand_choice2 = rng.gen_range(0..alvo.len());
+    while rand_choice1 == rand_choice2 {
+        rand_choice2 = rng.gen_range(0..alvo.len());
+    }
+    // println!("{:?}", alvo);
+    (alvo[rand_choice1], alvo[rand_choice2]) = (alvo[rand_choice2], alvo[rand_choice1]);
+    // println!("{:?}", alvo);
 }
--- gatsp/src/main.rs
+++ gatsp/src/main.rs
@@ -1,25 +1,65 @@
+use rand::Rng;
 use std::env;
 mod genetic;
 mod ler_arquivo_entrada;
 
-//cargo run nomearquivo tamanhodaentrada tampopulacao
+//cargo run nomearquivo tamanhodaentrada tampopulacao taxa_mutacao geracoes
+//cargo run att48.txt 48 10 0.2 5
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    //nome do arquivo de entrada
     let filename = &args[1];
-
+    //tamanho da entrada
     let tam = args[2].parse::<usize>().unwrap();
+    //tamanho da populacao
     let pop_size = args[3].parse::<usize>().unwrap();
+    //taxa de mutacao
+    let mutacao = args[4].parse::<f64>().unwrap();
+    //geracoes
+    let max_generations = args[5].parse::<usize>().unwrap();
+
+    //lendo informacoes da entrada, posicoes de cada cidade
     let mut cities = vec![[0.0; 2]; tam];
     ler_arquivo_entrada::ler_entrada(filename, &mut cities);
-    println!("Tamanho da entrada: {}", tam);
 
-    // deve ser mut tirei pro compilador nao encher o saco
-    let population = genetic::criar_populacao_inicial(tam, pop_size);
-    let fitness = genetic::calcular_fitness(population, tam, &mut cities);
-    println!("{:?}", fitness);
-    //selecao por roleta
-    //cruzamento, algum que funcione de preferencia sem utilizar coisas aleatorias
-    //mutacao
-    //remover o pior e adicionar o melhor
+    let mut contador: usize = 0;
+
+    //criando populacao inicial
+    let mut population = genetic::criar_populacao_inicial(tam, pop_size);
+
+    while contador < max_generations {
+        //calculando o fitness de cada cromossomo
+        let fitness = genetic::calcular_fitness(population.clone(), tam, &mut cities);
+        //idx do melhor cromossomo
+        let idx_melhor = genetic::melhor_cromossomo(fitness.clone());
+        println!("{:?}", population[idx_melhor]);
+        println!(
+            "{}",
+            genetic::value_of_cromossome(population[idx_melhor].clone(), tam, &mut cities)
+        );
+        //primeiro individuo selecionado
+        let idx1: i32 = genetic::selecao_por_roleta(fitness.clone());
+        //segundo individuo selecionado
+        let idx2: i32 = genetic::selecao_por_roleta(fitness.clone());
+        //melhor resultado do cruzamento entre os dois individuos selecionados
+        let mut filho: Vec<i32> = genetic::cruzamento_cx(
+            population[idx1 as usize].clone(),
+            population[idx2 as usize].clone(),
+            &mut cities,
+        );
+        //idx do pior cromossomo
+        let idx_pior = genetic::pior_cromossomo(fitness.clone());
+        //numero aleatorio para definir se vai ou nao acontecer a mutacao
+        let mut rng = rand::thread_rng();
+        let rand_choice = rng.gen_range(0.0..1.0);
+        if rand_choice <= mutacao {
+            genetic::mutacao(&mut filho);
+        }
+        //trocando o pior pelo novo individuo gerado
+
+        population[idx_pior] = filho;
+
+        contador += 1;
+    }
 }
